052867fc57a150aad23db5d1a72ffbd375bc3f2a
asuha <asuhacoder@gmail.com>
2020-11-17 19:21:15 +0900

    fix: convert type of jwt method #34

--- api/utility/jwt.go
+++ api/utility/jwt.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	uuid "github.com/satori/go.uuid"
 )
 
 // ParseJWT parse jwt and return uuid and permission
-func ParseJWT(tokenString string) (string, string, error) {
+func ParseJWT(tokenString string) (uuid.UUID, string, error) {
 	mySingningKey := []byte("AllYourBase")
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
@@ -18,11 +19,11 @@ func ParseJWT(tokenString string) (string, string, error) {
 	if err != nil {
 		panic(err)
 	}
-	var uuid string
+	var uuid uuid.UUID
 	var permission string
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		uuid = claims["uuid"]
-		permission = claims["permission"]
+		uuid = uuid.FromString(fmt.Sprintf("%v", claims["uuid"]))
+		permission = fmt.Sprintf("%v", claims["permission"])
 	}
 	return uuid, permission, nil
 }
